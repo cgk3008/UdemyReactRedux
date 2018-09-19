7d7c2584e481ddbf90e216f0c46c13580c9a126a
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-09-19 16:41:30 -0400

    Add project files.

---
+++ UdemyReactRedux/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UdemyReactRedux.MvcApplication" Language="C#" %>
---
+++ UdemyReactRedux/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UdemyReactRedux.Startup))]
+namespace UdemyReactRedux
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
