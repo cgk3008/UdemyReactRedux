bdd601ea155d9a9c3ea4036e540836aa5aa1fed7
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-09-25 22:59:20 -0400

    setup and new js files

--- UdemyReactRedux/Scripts/React/app.js
+++ UdemyReactRedux/Scripts/React/app.js
@@ -5,8 +5,22 @@ import ReactDOM from 'react-dom';
 //https://www.youtube.com/watch?v=bnFgGYooDCM
 
 
-ReactDOM.render(
-    <h1> Hello, world!</h1>,
-    document.getElementById('root')
-    //ok
-);
+//ReactDOM.render(
+//    <h1> Hello, world!</h1>,
+//    document.getElementById('root')
+//    //ok
+//);
+
+
+const API-KEY = 'add key here see private file in explorer'
+
+
+//"const" is an ES6 concept, this value will not change it is a constant!
+const App = () => {
+
+    return <div>Hi!</div>;  //this is JSX, looks like HTML but is javascript-webpack and babel transform this code into HTML
+}
+
+//step 2
+
+ReactDOM.render(<App />, document.querySelector('.container'));
